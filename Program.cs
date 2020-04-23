cd997e122651c3750900dbb4e510fc7d73e8fe5e
Liatmoss <liatmoss@hotmail.com>
2020-04-23 06:44:58 +0100

    Display fizz if divisible by 3 and buzz if divisible by 5

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 
 namespace Fizzbuzz
 {
-    class Program
+    class Fizzbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 100; i ++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                } 
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
