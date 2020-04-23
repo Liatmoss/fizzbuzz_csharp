583164a321d6f151b724db3e5635abb23fc1fb76
Liatmoss <liatmoss@hotmail.com>
2020-04-23 06:59:59 +0100

    Add fizzbuzz for 3 and 5

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace Fizzbuzz
         {
             for (int i = 0; i <= 100; i ++)
             {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
                 if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
@@ -22,5 +26,6 @@ namespace Fizzbuzz
                 }
             }
         }
+
     }
 }
